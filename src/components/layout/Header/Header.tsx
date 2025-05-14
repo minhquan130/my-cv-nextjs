60ffacc577dccddde7ec5d826f698fec2d4e5043
minhquan130 <minhq130@gmail.com>
2025-05-14 15:54:03 +0700

    update: update home page

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import AvatarPicture from '../../../../public/images/avatar.jpg'
-import Image from 'next/image'
 
 export default function Header() {
   return (
     <header className='bg-primary h-60 w-screen'>
         <div>
-            <Image src={AvatarPicture} alt='avatar' width={500} />
+            {/* <Image src={AvatarPicture} alt='avatar' width={500} /> */}
+            Hello Quỳnh Như
         </div>
     </header>
   )
